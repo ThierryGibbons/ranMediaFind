55ed0fbd2fe01850be109e4353e6bb3dd93116b5
Thierry <37156151+ThierryGibbons@users.noreply.github.com>
2021-03-17 22:05:02 +1300

    finds random .jonVid file within directory

--- zza.py
+++ zza.py
@@ -1,7 +1,16 @@
-#   TODO: Read folder & file names
-#   TODO: Find random movie/ tv show/ media to watch and open file
-#   TODO
+#   TODO: Create GUI
 
-import os
-import system
 import random
+import glob
+
+#   create vids list
+vids = []
+
+
+#   grab all files inside ./media with the file extension .jonVid
+for jonVid in glob.glob("./media/**/*.jonVid", recursive = True): 
+    #   pack every occurance into the list vids
+    vids.append(jonVid)
+
+#   print random item from list
+print(vids[random.randint(0, len(vids))])
