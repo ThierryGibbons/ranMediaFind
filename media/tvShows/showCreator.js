14002a1bc8f0347de7646490821c53811cd3da2a
Thierry <37156151+ThierryGibbons@users.noreply.github.com>
2021-03-17 20:12:43 +1300

    added more jonVids

--- media/tvShows/showCreator.js
+++ media/tvShows/showCreator.js
@@ -2,20 +2,21 @@
 /*jshint node: true*/
 
 const fs = require('fs');
+argv = require("yargs").argv;
 
 function epC(name) {
   i = 1;
+  fs.mkdirSync('./' + name);
   while (i < 11) {
-    console.log(i);
-    fs.writeFile(name + i + '.jonVid', 'sushi', (err) => {
+    fs.writeFile('./' + name + '/' + name + ' ' + i + '.jonVid', 'sushi', (err) => {
       if (err) {
         console.log('err: ', err);
       } else {
-        console.log('chur');
       }
     });
     i += 1;
   }
+  console.log('show printed');
 }
 
-epC('cool');
+epC(argv.show);
